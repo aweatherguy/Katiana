7428dedddb0654a984afacec7b34df4fd41ed5c7
mkozlowski <10508687+m-kozlowski@users.noreply.github.com>
2019-02-23 03:04:02 +0100

    fix compilation with LUFA >= 16f4492eb4783f3821155d87907ad0db7be9cc90

--- Descriptors.c
+++ Descriptors.c
@@ -147,7 +147,7 @@ const USB_Descriptor_Configuration_t ConfigurationDescriptor =
 
 	.CDC_Functional_Header =
 		{
-			.Header                 = {.Size = sizeof(USB_CDC_Descriptor_FunctionalHeader_t), .Type = DTYPE_CSInterface},
+			.Header                 = {.Size = sizeof(USB_CDC_Descriptor_FunctionalHeader_t), .Type = CDC_DTYPE_CSInterface},
 			.Subtype                = 0x00,
 
 			.CDCSpecification       = VERSION_BCD(1,1,0),
@@ -155,7 +155,7 @@ const USB_Descriptor_Configuration_t ConfigurationDescriptor =
 
 	.CDC_Functional_ACM =
 		{
-			.Header                 = {.Size = sizeof(USB_CDC_Descriptor_FunctionalACM_t), .Type = DTYPE_CSInterface},
+			.Header                 = {.Size = sizeof(USB_CDC_Descriptor_FunctionalACM_t), .Type = CDC_DTYPE_CSInterface},
 			.Subtype                = 0x02,
 
 			.Capabilities           = 0x02,
@@ -163,7 +163,7 @@ const USB_Descriptor_Configuration_t ConfigurationDescriptor =
 
 	.CDC_Functional_Union =
 		{
-			.Header                 = {.Size = sizeof(USB_CDC_Descriptor_FunctionalUnion_t), .Type = DTYPE_CSInterface},
+			.Header                 = {.Size = sizeof(USB_CDC_Descriptor_FunctionalUnion_t), .Type = CDC_DTYPE_CSInterface},
 			.Subtype                = 0x06,
 
 			.MasterInterfaceNumber  = INTERFACE_ID_CDC_CCI,
